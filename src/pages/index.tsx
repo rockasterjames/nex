87c2f31b839bd545873538a6d41693f542552bcd
rockasterjames <166325246+rockasterjames@users.noreply.github.com>
2024-04-07 22:57:05 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home | Namada Explorers</title>
+        <title>Home</title>
         <meta name="description" content="Home | Namada Explorer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
