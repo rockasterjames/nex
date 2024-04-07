3d42c6d631c37032731e46d19e79e0ebb10a6e75
rockasterjames <166325246+rockasterjames@users.noreply.github.com>
2024-04-07 22:55:57 +0300

    Update index.tsx

--- src/pages/proposals/index.tsx
+++ src/pages/proposals/index.tsx
@@ -172,7 +172,7 @@ export default function Proposals() {
   return (
     <>
       <Head>
-        <title>Proposals | Namada Explorer</title>
+        <title>Proposals</title>
         <meta name="description" content="Proposals | Namada Explorer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
