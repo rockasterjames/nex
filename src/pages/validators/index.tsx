d62c13e47cc7e9bb9ef0b1c191fe703dd5b61681
rockasterjames <166325246+rockasterjames@users.noreply.github.com>
2024-04-07 22:56:42 +0300

    Update index.tsx

--- src/pages/validators/index.tsx
+++ src/pages/validators/index.tsx
@@ -185,7 +185,7 @@ export default function Validators() {
   return (
     <>
       <Head>
-        <title>Blocks | Namada Explorer</title>
+        <title>Blocks</title>
         <meta name="description" content="Blocks | Namada Explorer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
