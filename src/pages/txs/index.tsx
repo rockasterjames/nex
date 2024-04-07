33ce8bf81e3337d9e8fb41b8726f1ae08431d7d0
rockasterjames <166325246+rockasterjames@users.noreply.github.com>
2024-04-07 22:56:19 +0300

    Update index.tsx

--- src/pages/txs/index.tsx
+++ src/pages/txs/index.tsx
@@ -98,7 +98,7 @@ export default function Transactions() {
   return (
     <>
       <Head>
-        <title>Transactions | Namada Explorer</title>
+        <title>Transactions</title>
         <meta name="description" content="Transactions | Namada Explorer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
