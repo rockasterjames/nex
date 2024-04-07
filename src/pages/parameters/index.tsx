034cda50fa5cc7b70ca95956a2d50eaea937ab50
rockasterjames <166325246+rockasterjames@users.noreply.github.com>
2024-04-07 22:55:09 +0300

    Update index.tsx

--- src/pages/parameters/index.tsx
+++ src/pages/parameters/index.tsx
@@ -23,7 +23,7 @@ export default function Parameters() {
   return (
     <>
       <Head>
-        <title>Parameters | Namada Explorer</title>
+        <title>Parameters</title>
         <meta name="description" content="Parameters | Namada Explorer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
